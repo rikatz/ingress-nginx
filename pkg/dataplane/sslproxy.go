2a18548085f8dceeb6e8f33041e2bdb653dd5419
Ricardo Pchevuzinske Katz <ricardo.katz@gmail.com>
2022-07-23 16:15:58 -0300

    Initial work splitting cp and dp

---
+++ pkg/dataplane/sslproxy.go
@@ -0,0 +1,78 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dataplane
+
+import (
+	"fmt"
+	"net"
+
+	proxyproto "github.com/armon/go-proxyproto"
+	"k8s.io/ingress-nginx/internal/ingress/controller/config"
+	"k8s.io/ingress-nginx/pkg/tcpproxy"
+
+	"k8s.io/klog/v2"
+)
+
+func (n *NGINXConfigurer) setupSSLProxy() {
+	// TODO: Get the Configuration below via gRPC: do a GetConfiguration that returns the running ConfigMap from Control Plane
+	cfg := config.Configuration{}
+	sslPort := n.cfg.ListenPorts.HTTPS
+	proxyPort := n.cfg.ListenPorts.SSLProxy
+
+	klog.InfoS("Starting TLS proxy for SSL Passthrough")
+	n.Proxy = &tcpproxy.TCPProxy{
+		Default: &tcpproxy.TCPServer{
+			Hostname:      "localhost",
+			IP:            "127.0.0.1",
+			Port:          proxyPort,
+			ProxyProtocol: true,
+		},
+	}
+
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%v", sslPort))
+	if err != nil {
+		klog.Fatalf("%v", err)
+	}
+
+	proxyList := &proxyproto.Listener{Listener: listener, ProxyHeaderTimeout: cfg.ProxyProtocolHeaderTimeout}
+
+	// accept TCP connections on the configured HTTPS port
+	go func() {
+		for {
+			var conn net.Conn
+			var err error
+
+			// TODO: Understand this thing :)
+			//if n.store.GetBackendConfiguration().UseProxyProtocol {
+			if cfg.UseProxyProtocol {
+				// wrap the listener in order to decode Proxy
+				// Protocol before handling the connection
+				conn, err = proxyList.Accept()
+			} else {
+				conn, err = listener.Accept()
+			}
+
+			if err != nil {
+				klog.Warningf("Error accepting TCP connection: %v", err)
+				continue
+			}
+
+			klog.V(3).InfoS("Handling TCP connection", "remote", conn.RemoteAddr(), "local", conn.LocalAddr())
+			go n.Proxy.Handle(conn)
+		}
+	}()
+}
---
+++ pkg/dataplane/types.go
@@ -0,0 +1,108 @@
+/*
+Copyright 2022 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dataplane
+
+import (
+	"net"
+	"sync"
+	"time"
+
+	"k8s.io/client-go/util/flowcontrol"
+	ngxconfig "k8s.io/ingress-nginx/internal/ingress/controller/config"
+	"k8s.io/ingress-nginx/internal/ingress/controller/ingressclass"
+	ngxtemplate "k8s.io/ingress-nginx/internal/ingress/controller/template"
+	"k8s.io/ingress-nginx/internal/ingress/metric"
+	"k8s.io/ingress-nginx/internal/ingress/metric/collectors"
+	"k8s.io/ingress-nginx/internal/task"
+	"k8s.io/ingress-nginx/pkg/apis/ingress"
+	"k8s.io/ingress-nginx/pkg/tcpproxy"
+)
+
+// Configuration contains all the settings required by an Ingress controller
+type Configuration struct {
+	ResyncPeriod time.Duration
+
+	HealthCheckHost string
+	ListenPorts     *ngxconfig.ListenPorts
+
+	DisableServiceExternalName bool
+
+	EnableSSLPassthrough bool
+
+	EnableProfiling bool
+
+	EnableMetrics  bool
+	MetricsPerHost bool
+	MetricsBuckets *collectors.HistogramBuckets
+
+	FakeCertificate *ingress.SSLCert
+
+	SyncRateLimit float32
+
+	IngressClassConfiguration *ingressclass.IngressClassConfiguration
+
+	GlobalExternalAuth  *ngxconfig.GlobalExternalAuth
+	MaxmindEditionFiles *[]string
+
+	MonitorMaxBatchSize int
+
+	PostShutdownGracePeriod int
+	ShutdownGracePeriod     int
+
+	DeepInspector bool
+
+	DynamicConfigurationRetries int
+}
+
+// NGINXConfigurer describes a NGINX Ingress controller.
+type NGINXConfigurer struct {
+	cfg *Configuration
+
+	syncRateLimiter flowcontrol.RateLimiter
+
+	// syncQueue will be used to trigger new full reconciliations when required
+	syncQueue *task.Queue
+
+	// configureLock is a mutex to avoid race conditions during configuration that needs to change nginx.conf
+	configureLock *sync.Mutex
+
+	// stopLock is used to enforce that only a single call to Stop send at
+	// a given time. We allow stopping through an HTTP endpoint and
+	// allowing concurrent stoppers leads to stack traces.
+	stopLock *sync.Mutex
+
+	stopCh chan struct{}
+	// ngxErrCh is used to detect errors with the NGINX processes
+	ngxErrCh chan error
+
+	// runningConfig contains the running configuration in the Backend
+	runningConfig *ingress.Configuration
+
+	t ngxtemplate.Writer
+
+	resolver []net.IP
+
+	isIPV6Enabled bool
+
+	isShuttingDown bool
+
+	Proxy *tcpproxy.TCPProxy
+
+	metricCollector metric.Collector
+
+	command NginxExecTester
+}
